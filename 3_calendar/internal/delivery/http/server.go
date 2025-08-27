99a499f17806dc4eb940379d683933efc9129756
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-27 20:11:01 +0300

    add HTTP server setup: logging capabilities and a health check route

--- 3_calendar/internal/delivery/http/server.go
+++ 3_calendar/internal/delivery/http/server.go
@@ -1,7 +1,42 @@
 package http
 
-import "github.com/labstack/echo/v4"
+import (
+	"calendar/internal/models/log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
 
 type Server struct {
-	e *echo.Echo
+	e    *echo.Echo
+	logs chan<- log.Entry
+}
+
+func NewServer(logs chan<- log.Entry) *Server {
+	e := echo.New()
+	s := &Server{
+		e:    e,
+		logs: logs,
+	}
+
+	s.setup()
+
+	return s
+}
+
+func (s *Server) setup() {
+	s.e.Use(middleware.Recover())
+
+	s.setupRoutes()
+}
+
+func (s *Server) setupRoutes() {
+	s.e.GET("/health", func(c echo.Context) error {
+		return c.NoContent(http.StatusOK)
+	})
+}
+
+func (s *Server) Start(addr string) error {
+	return s.e.Start(addr)
 }
