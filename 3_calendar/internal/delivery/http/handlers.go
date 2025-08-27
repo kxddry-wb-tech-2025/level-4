59aa4d2b1c2b237db7d199a1a9405f6c0320c0bf
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-27 22:41:31 +0300

    add handlers

--- 3_calendar/internal/delivery/http/handlers.go
+++ 3_calendar/internal/delivery/http/handlers.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"calendar/internal/models"
+	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -25,17 +26,54 @@ func (s *Server) createEvent(c echo.Context) error {
 }
 
 func (s *Server) getEvents(c echo.Context) error {
-	return nil
+	events, err := s.svc.GetEvents(c.Request().Context())
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, events)
 }
 
 func (s *Server) getEvent(c echo.Context) error {
-	return nil
+	id := c.Param("id")
+	event, err := s.svc.GetEvent(c.Request().Context(), id)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, event)
 }
 
 func (s *Server) updateEvent(c echo.Context) error {
-	return nil
+	id := c.Param("id")
+	var req models.UpdateEventRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, echo.Map{"error": err.Error()})
+	}
+	if err := c.Validate(&req); err != nil {
+		return c.JSON(http.StatusUnprocessableEntity, echo.Map{"error": err.Error()})
+	}
+
+	event, err := s.svc.UpdateEvent(c.Request().Context(), id, req)
+	if err != nil {
+		if errors.Is(err, models.ErrNotFound) {
+			return c.JSON(http.StatusNotFound, echo.Map{"error": err.Error()})
+		}
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, event)
 }
 
 func (s *Server) deleteEvent(c echo.Context) error {
-	return nil
+	id := c.Param("id")
+	err := s.svc.DeleteEvent(c.Request().Context(), id)
+	if err != nil {
+		if errors.Is(err, models.ErrNotFound) {
+			return c.JSON(http.StatusNotFound, echo.Map{"error": err.Error()})
+		}
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
+	}
+
+	return c.NoContent(http.StatusOK)
 }
