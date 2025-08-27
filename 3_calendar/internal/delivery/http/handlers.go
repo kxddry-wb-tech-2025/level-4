3ebbde6b1573f62fcdeaff185d9e8d75c53a9905
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-27 22:45:33 +0300

    add logging in handlers

--- 3_calendar/internal/delivery/http/handlers.go
+++ 3_calendar/internal/delivery/http/handlers.go
@@ -2,11 +2,18 @@ package http
 
 import (
 	"calendar/internal/models"
+	"calendar/internal/models/log"
 	"errors"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
+
+func (s *Server) sendLog(entry log.Entry) {
+	go func() {
+		s.logs <- entry
+	}()
+}
 
 func (s *Server) createEvent(c echo.Context) error {
 	var req models.CreateEventRequest
@@ -19,6 +26,10 @@ func (s *Server) createEvent(c echo.Context) error {
 
 	event, err := s.svc.CreateEvent(c.Request().Context(), req)
 	if err != nil {
+		s.sendLog(log.Error(err, "failed to create event", echo.Map{
+			"request_id": c.Response().Header().Get(echo.HeaderXRequestID),
+			"op":         "createEvent",
+		}))
 		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
 	}
 
@@ -28,6 +39,10 @@ func (s *Server) createEvent(c echo.Context) error {
 func (s *Server) getEvents(c echo.Context) error {
 	events, err := s.svc.GetEvents(c.Request().Context())
 	if err != nil {
+		s.sendLog(log.Error(err, "failed to get events", echo.Map{
+			"request_id": c.Response().Header().Get(echo.HeaderXRequestID),
+			"op":         "getEvents",
+		}))
 		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
 	}
 
@@ -38,6 +53,15 @@ func (s *Server) getEvent(c echo.Context) error {
 	id := c.Param("id")
 	event, err := s.svc.GetEvent(c.Request().Context(), id)
 	if err != nil {
+		if errors.Is(err, models.ErrNotFound) {
+			return c.JSON(http.StatusNotFound, echo.Map{"error": err.Error()})
+		}
+
+		s.sendLog(log.Error(err, "failed to get event", echo.Map{
+			"id":         id,
+			"request_id": c.Response().Header().Get(echo.HeaderXRequestID),
+			"op":         "getEvent",
+		}))
 		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
 	}
 
@@ -59,6 +83,12 @@ func (s *Server) updateEvent(c echo.Context) error {
 		if errors.Is(err, models.ErrNotFound) {
 			return c.JSON(http.StatusNotFound, echo.Map{"error": err.Error()})
 		}
+
+		s.sendLog(log.Error(err, "failed to update event", echo.Map{
+			"id":         id,
+			"request_id": c.Response().Header().Get(echo.HeaderXRequestID),
+			"op":         "updateEvent",
+		}))
 		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
 	}
 
@@ -72,6 +102,12 @@ func (s *Server) deleteEvent(c echo.Context) error {
 		if errors.Is(err, models.ErrNotFound) {
 			return c.JSON(http.StatusNotFound, echo.Map{"error": err.Error()})
 		}
+
+		s.sendLog(log.Error(err, "failed to delete event", echo.Map{
+			"id":         id,
+			"request_id": c.Response().Header().Get(echo.HeaderXRequestID),
+			"op":         "deleteEvent",
+		}))
 		return c.JSON(http.StatusInternalServerError, echo.Map{"error": err.Error()})
 	}
 
