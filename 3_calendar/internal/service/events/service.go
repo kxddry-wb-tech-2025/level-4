c4d066580d278b19222ea6ea4ac820e040c9add1
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-27 23:06:49 +0300

    add the simplest service

---
+++ 3_calendar/internal/service/events/service.go
@@ -0,0 +1,67 @@
+package events
+
+import (
+	"calendar/internal/models"
+	"context"
+)
+
+type EventRepository interface {
+	Create(event models.CreateEventRequest) (string, error)
+	GetAll() ([]models.Event, error)
+	Get(id string) (models.Event, error)
+	Update(id string, event models.UpdateEventRequest) error
+	Delete(id string) error
+}
+
+type Service struct {
+	repo EventRepository
+}
+
+func NewService(repo EventRepository) *Service {
+	return &Service{repo: repo}
+}
+
+func (s *Service) CreateEvent(ctx context.Context, event models.CreateEventRequest) (string, error) {
+	id, err := s.repo.Create(event)
+	if err != nil {
+		return "", err
+	}
+
+	return id, nil
+}
+
+func (s *Service) GetEvents(ctx context.Context) ([]models.Event, error) {
+	events, err := s.repo.GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	return events, nil
+}
+
+func (s *Service) GetEvent(ctx context.Context, id string) (models.Event, error) {
+	event, err := s.repo.Get(id)
+	if err != nil {
+		return models.Event{}, err
+	}
+
+	return event, nil
+}
+
+func (s *Service) UpdateEvent(ctx context.Context, id string, event models.UpdateEventRequest) error {
+	err := s.repo.Update(id, event)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Service) DeleteEvent(ctx context.Context, id string) error {
+	err := s.repo.Delete(id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
