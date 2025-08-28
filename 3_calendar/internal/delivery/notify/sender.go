ae47a46e620040b457440e6a0701a616db2bca3d
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-28 11:15:19 +0300

    add email sender adapter

--- 3_calendar/internal/delivery/notify/sender.go
+++ 3_calendar/internal/delivery/notify/sender.go
@@ -1,4 +1,41 @@
 package notify
 
+import (
+	"calendar/internal/config"
+	"calendar/internal/delivery"
+	"calendar/internal/models"
+	"context"
+	"fmt"
+)
+
 type EmailClient interface {
+	SendEmail(ctx context.Context, msg *delivery.EmailMessage) error
+}
+
+type Sender struct {
+	emailClient EmailClient
+	config      *config.EmailConfig
+}
+
+func NewSender(emailClient EmailClient, config *config.EmailConfig) *Sender {
+	return &Sender{
+		emailClient: emailClient,
+		config:      config,
+	}
+}
+
+func (s *Sender) Send(ctx context.Context, notification models.Notification) error {
+	switch notification.Channel {
+	case "email":
+		return s.emailClient.SendEmail(ctx, &delivery.EmailMessage{
+			From:      s.config.Username,
+			To:        []string{notification.Recipient},
+			Subject:   notification.Message,
+			Body:      notification.Message,
+			IsHTML:    true,
+			FilePaths: nil,
+		})
+	default:
+		return fmt.Errorf("unsupported channel: %s", notification.Channel)
+	}
 }
