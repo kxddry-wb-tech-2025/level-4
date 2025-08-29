35f76af58f8c9a90bbe3fb58c0317b066a7194e2
Ivan Kudryavtsev <ivakudry@gmail.com>
2025-08-29 17:25:52 +0300

    add grafana and prometheus

--- 4_metrics/cmd/app/main.go
+++ 4_metrics/cmd/app/main.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"context"
 	"log"
 	"metrics/internal/delivery/http"
-	"metrics/internal/metrics"
 	"os"
-	"time"
 
 	"github.com/joho/godotenv"
 )
-
-const MetricsDuration = 2 * time.Second
 
 func main() {
 	godotenv.Load()
@@ -20,15 +15,9 @@ func main() {
 	if port == "" {
 		port = "2112"
 	}
-
-	ctx, cancel := context.WithCancel(context.Background())
 	server := http.New()
-	go metrics.RecordMetrics(ctx, MetricsDuration)
 
 	if err := server.Start(port); err != nil {
 		log.Printf("Failed to start server: %v", err)
 	}
-
-	cancel()
-	time.Sleep(MetricsDuration)
 }
